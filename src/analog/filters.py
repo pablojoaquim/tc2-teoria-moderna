1eae3d80a48b84276637268c8806da0e3b783b3e
Pablo Joaquim <pablo.joaquim@famar.com>
2023-03-25 01:31:21 -0300

    Now the filter time response can be evaluated generating an input signal and using lsim to obtain the output of the system

--- src/analog/filters.py
+++ src/analog/filters.py
@@ -10,7 +10,9 @@
 # ******************************************************************************
 # * import modules
 # ******************************************************************************
+import numpy as np
 from scipy import signal as filters
+from scipy.signal import lsim
 
 # ******************************************************************************
 # * Objects Declarations
@@ -55,6 +57,62 @@ class Butterworth():
     def butter_bandstop(wci, wcs, order=5):
         return filters.butter(order, [wci, wcs], btype='bandstop', analog=True)
 
+class SignalGenerator():
+    # ******************************************************************************
+    # * @brief The __init__() function is called automatically every time the class
+    # * is being used to create a new object.
+    # ******************************************************************************
+    def __init__(self):
+        # The self parameter is a reference to the current instance of the class,
+        # and is used to access variables that belong to the class.
+        pass
+
+    # ******************************************************************************
+    # * @brief Returns a string as a representation of the object.
+    # ******************************************************************************
+    def __repr__(self):
+        return '<Metadata(name={self.id!r})>'.format(self=self)
+    
+    # ******************************************************************************
+    # * @brief Create a signal based in the time data and the several fourier components 
+    # * defined in the signals input in the form [[A,w,phi]]
+    # ******************************************************************************    
+    def signal(signals, step, tmin, tmax):
+        t = np.linspace(tmin, step, tmax, endpoint=False)
+        u = 0
+        for signal in signals:
+            A=signal[0]
+            w=signal[1]
+            phi=signal[2]
+            u = u + A*np.sin(2*np.pi*w*t)
+            
+        return u,t
+
+class ApplyFilter():
+    # ******************************************************************************
+    # * @brief The __init__() function is called automatically every time the class
+    # * is being used to create a new object.
+    # ******************************************************************************
+    def __init__(self):
+        # The self parameter is a reference to the current instance of the class,
+        # and is used to access variables that belong to the class.
+        pass
+
+    # ******************************************************************************
+    # * @brief Returns a string as a representation of the object.
+    # ******************************************************************************
+    def __repr__(self):
+        return '<Metadata(name={self.id!r})>'.format(self=self)
+    
+    # ******************************************************************************
+    # * @brief Create a signal based in the time data and the several fourier components 
+    # * defined in the signals input in the form [[A,w,phi]]
+    # ******************************************************************************    
+    def eval(h, input, t):
+        num = h[0]
+        den = h[1]
+        tout, output, xout = lsim((num, den), U=input, T=t)
+        return output
 
 # ******************************************************************************
 # * Object and variables Definitions
--- src/main.py
+++ src/main.py
@@ -106,92 +106,19 @@ if __name__ == '__main__':
         # plotter.plot(H, [fo,-3], "Butterworth - bandstop")
         # plotter.pzplot(H, wc, title="Butterworth - bandstop")        
         
+        # Test a filter time response
         # The input signal is the sum of three sinusoidal curves, with frequencies 4 Hz, 40 Hz, and 80 Hz. 
         # The filter should mostly eliminate the 40 Hz and 80 Hz components, leaving just the 4 Hz signal.
         num, den = filters.Butterworth.butter_lowpass(wc=2*np.pi*12, order=5)
-        t = np.linspace(0, 1.25, 500, endpoint=False)
-        u = (np.cos(2*np.pi*4*t) + 0.6*np.sin(2*np.pi*40*t) + 0.5*np.cos(2*np.pi*80*t))
-        
-        # Simulate the filter with lsim.
-        tout, yout, xout = lsim((num, den), U=u, T=t)
-        plt.plot(t, u, 'r', alpha=0.5, linewidth=1, label='input')
-        plt.plot(tout, yout, 'k', linewidth=1.5, label='output')
+        h = [num, den]
+        inputSignal,t = filters.SignalGenerator.signal([[1,4,np.pi/2], [0.6,40,0], [0.5,80,np.pi/2]], 1.25, 0, 500)
+        outputSignal = filters.ApplyFilter.eval(h, inputSignal, t)
+        plt.plot(t, inputSignal, 'r', alpha=0.5, linewidth=1, label='input')
+        plt.plot(t, outputSignal, 'k', linewidth=1.5, label='output')
         plt.legend(loc='best', shadow=True, framealpha=1)
         plt.grid(alpha=0.3)
         plt.xlabel('t')
         plt.show()
-        
-        
-        
-        # b1, a1 = filters.butter(1, 1, 'high', analog=True)
-        # print("analog filter: ", [b1, a1])
-
-        # # Sample rate and desired cutoff frequencies (in Hz).
-        # fs = 5000.0
-        # lowcut = 500.0
-        # highcut = 1250.0
-
-        # # Plot the frequency response for a few different orders.
-        # plt.figure(1)
-        # plt.clf()
-        
-        # orders = [3, 6, 9]
-        # for order in orders:
-        #     b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-        #     w, h = filters.freqz(b, a, fs=fs, worN=2000)
-        #     plt.plot(w, abs(h), label="order = %d" % order)
-
-        # plt.plot([0, 0.5 * fs], [np.sqrt(0.5), np.sqrt(0.5)],
-        #         '--', label='sqrt(0.5)')
-        # plt.xlabel('Frequency (Hz)')
-        # plt.ylabel('Gain')
-        # plt.grid(True)
-        # plt.legend(loc='best')
-
-        # orders = [3, 6, 9]
-        # for order in orders:
-        #     b, a = butter_lowpass(lowcut, fs, order=order)
-        #     w, h = filters.freqz(b, a, fs=fs, worN=2000)
-        #     plt.plot(w, abs(h), label="order = %d" % order)
-
-        # plt.plot([0, 0.5 * fs], [np.sqrt(0.5), np.sqrt(0.5)],
-        #         '--', label='sqrt(0.5)')
-        # plt.xlabel('Frequency (Hz)')
-        # plt.ylabel('Gain')
-        # plt.grid(True)
-        # plt.legend(loc='best')
-
-        # # Filter a noisy signal.
-        # T = 1
-        # nsamples = T * fs
-        # t = np.arange(0, nsamples) / fs
-        # a = 0.02
-        # f0 = 600.0
-        # x = 0.1 * np.sin(2 * np.pi * 1.2 * np.sqrt(t))
-        # x += 0.01 * np.cos(2 * np.pi * 312 * t + 0.1)
-        # x += a * np.cos(2 * np.pi * f0 * t + .11)
-        # x += 0.03 * np.cos(2 * np.pi * 2000 * t)
-        # plt.figure(2)
-        # plt.clf()
-        # plt.plot(t, x, label='Noisy signal')
-
-        # y = butter_bandpass_filter(x, lowcut, highcut, fs, order=6)
-        # plt.plot(t, y, label='Filtered signal (%g Hz)' % f0)
-        # plt.xlabel('time (seconds)')
-        # plt.hlines([-a, a], 0, T, linestyles='--')
-        # plt.grid(True)
-        # plt.axis('tight')
-        # plt.legend(loc='upper left')
-
-        # y = butter_lowpass_filter(x, lowcut, fs, order=6)
-        # plt.plot(t, y, label='Filtered signal (%g Hz)' % f0)
-        # plt.xlabel('time (seconds)')
-        # plt.hlines([-a, a], 0, T, linestyles='--')
-        # plt.grid(True)
-        # plt.axis('tight')
-        # plt.legend(loc='upper left')
-        
-        # plt.show()
         
     except RuntimeError:
         print("Finishing...", flush=True)
